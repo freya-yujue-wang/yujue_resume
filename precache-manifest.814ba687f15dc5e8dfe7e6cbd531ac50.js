da52e77f775c9da3ca04abab6f7dc132b05a2c8c
freya-yujue-wang <yujuewa@gmail.com>
2020-08-21 02:45:01 +0000

    Deploying to gh-pages from  @ c8186d44b6790afdaa3869168a18db340b482f0f 🚀

---
+++ precache-manifest.814ba687f15dc5e8dfe7e6cbd531ac50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb10203294e8a3c30abb1fac3a7ccf42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b282c3f46a709d5d231",
+    "url": "/static/css/main.7ecf0735.chunk.css"
+  },
+  {
+    "revision": "50b6558433f98cf68ba2",
+    "url": "/static/js/2.f33d5424.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f33d5424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b282c3f46a709d5d231",
+    "url": "/static/js/main.4db11cd7.chunk.js"
+  },
+  {
+    "revision": "43a62b9c014d0ba94a80",
+    "url": "/static/js/runtime-main.60a07d1e.js"
+  }
+]);
